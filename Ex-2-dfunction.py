8fcc3ec22e9dabd53850255762cffca87be1992b
priyakaja <priya.ammu0219@gmail.com>
2025-05-19 14:05:21 +0530

    2nd commit

---
+++ Ex-2-dfunction.py
@@ -0,0 +1,5 @@
+def double(n):
+    return 2*n
+print("To Call this function using its name and assign the values of you")
+print("The program ran successfully")
+print("First modification for 2nd comit")
