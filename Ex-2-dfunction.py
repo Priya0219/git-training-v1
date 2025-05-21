a08f1db05d78367ebd74bcdf0ffddf2d2e78b600
Priya Kaja <137280393+Priya0219@users.noreply.github.com>
2025-05-21 11:15:12 +0530

    Update Ex-2 from the github
    Update Ex-2 from the GitHub website by the print statement for the first time.

--- Ex-2-dfunction.py
+++ Ex-2-dfunction.py
@@ -3,3 +3,5 @@ def double(n):
 print("To Call this function using its name and assign the values of you")
 print("The program ran successfully")
 print("First modification for 2nd comit")
+
+print("Added this line from the GitHub website for the 1st time")
