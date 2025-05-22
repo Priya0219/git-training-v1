581e1993d6b93363d2a800f710c7030522ded0a5
ravindrag-97 <ravindrag@eficens.com>
2025-05-22 12:09:26 +0530

    Changes the files ex1 and ex2 with x = 100 and y = 200

--- Ex-2-dfunction.py
+++ Ex-2-dfunction.py
@@ -5,3 +5,6 @@ print("The program ran successfully")
 print("First modification for 2nd comit")
 
 print("Added this line from the GitHub website for the 1st time")
+#----------------
+print("Added this line from user3")
+y = 200
