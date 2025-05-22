3346bad68f87a2b63d805d383b59ebba1b585313
ravindrag-97 <ravindrag@eficens.com>
2025-05-22 14:02:07 +0530

    Update Ex-2 with b=100 & c=200

--- Ex-2-dfunction.py
+++ Ex-2-dfunction.py
@@ -8,3 +8,7 @@ print("Added this line from the GitHub website for the 1st time")
 #----------------
 print("Added this line from user3")
 y = 200
+#---------
+print("Added this line from user3")
+b = 100
+c = 200
